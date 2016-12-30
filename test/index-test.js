4326d4e62f974a316a1421bd1c85a96873648e64
Jeff McAffer <jeff.mcaffer@microsoft.com>
2016-12-30 10:28:56 -0800

    update license and copyrights

--- test/index-test.js
+++
@@ -1,9 +0,0 @@
-// const Q = require('q');
-const chai = require('chai');
-// const sinon = require('sinon');
-chai.should();
-
-// const config = require('../index');
-
-describe('RedisConfigStore', () => {
-});
